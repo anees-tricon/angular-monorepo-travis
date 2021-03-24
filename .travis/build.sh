57240e7e58cbefe7571b2cc648ae9c0cd027df0c
Anees Mohamed <anees@triconinfotech.com>
2021-03-24 20:53:54 +0530

    build: improve build and try csd only

--- .travis/build.sh
+++ .travis/build.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
 if [[ -z $1 ]]; then
-    echo "Taget cannot be emtpy"
+    echo "Project cannot be emtpy"
     exit 1
 fi
 
 # $1 is the target which we pass from .travis.yml file. projects/csd or projects/librarian
-echo "Building Project $1"
+#echo "Building Project $1"
 
 # Split the target with / character
-ARRTARGET=(${1//// })
+#ARRTARGET=(${1//// })
 
 # Get the project to run the scripts on.
-PROJECT=${ARRTARGET[1]}
+#PROJECT=${ARRTARGET[1]}
 
 # Run the scripts that needs to be run on the project
-ng lint --project=${PROJECT}
+npm install
+ng lint --project=${1}
