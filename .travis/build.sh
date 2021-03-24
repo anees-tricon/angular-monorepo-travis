d88a50f80f20bda94d3f2fad2ef506564db07f91
Anees Mohamed <anees@triconinfotech.com>
2021-03-24 20:40:40 +0530

    build: fix package json and add build script

---
+++ .travis/build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if [[ -z $1 ]]; then
+    echo "Taget cannot be emtpy"
+    exit 1
+fi
+
+# $1 is the target which we pass from .travis.yml file. projects/csd or projects/librarian
+echo "Building Project $1"
+
+# Split the target with / character
+ARRTARGET=(${1//// })
+
+# Get the project to run the scripts on.
+PROJECT=${ARRTARGET[1]}
+
+# Run the scripts that needs to be run on the project
+ng lint --project=${PROJECT}
